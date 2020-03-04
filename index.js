caa982c43446d0e5adf37ad901d3609560f26295
Cesar Murillo <namiayumis@gmail.com>
2020-03-04 12:45:00 +0900

    configurando express

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("desde index.js");
+const express = require("express");
+
+// crear el servidor
+const app = express();
+
+//puerto de la app
+const PORT = process.env.PORT || 4000;
+
+//definir la pagina principal
+app.get("/", (req, res) => {
+  res.send("Hola Mundo");
+});
+
+// arrancar la app
+app.listen(PORT, () => {
+  console.log(`El servidor esta funcionando en el puerto ${PORT}`);
+});
