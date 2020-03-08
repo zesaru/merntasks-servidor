41f66099debed52d0ce76d30c4428dc134bfdacb
Cesar Murillo <namiayumis@gmail.com>
2020-03-08 21:53:50 +0900

    controller y routing proyectos

---
+++ routes/proyecto.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+const proyectoController = require("../controllers/proyectoController");
+
+// Crea proyectos
+// api/proyectos
+router.post("/", proyectoController.crearProyecto);
+
+module.exports = router;
