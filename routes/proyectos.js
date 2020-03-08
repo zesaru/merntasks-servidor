962a2ce15c042cdc10f3786f678b7c1bb7f1e8ab
Cesar Murillo <namiayumis@gmail.com>
2020-03-08 22:52:44 +0900

    custom middleware y agregar usuario al proyecto la persona que lo creo

--- routes/proyectos.js
+++ routes/proyectos.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const proyectoController = require("../controllers/proyectoController");
+const auth = require("../middleware/auth");
 
 // Crea proyectos
 // api/proyectos
-router.post("/", proyectoController.crearProyecto);
+router.post("/", auth, proyectoController.crearProyecto);
+
+router.get("/", auth, proyectoController.crearProyecto);
 
 module.exports = router;
