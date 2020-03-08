d89121a955dda2e7240f929d4df609daf149bd99
Cesar Murillo <namiayumis@gmail.com>
2020-03-08 20:50:57 +0900

    revisando si el usuario existe, si el password es correcto o incorrecto

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ const PORT = process.env.PORT || 4000;
 
 // Importar rutas
 app.use("/api/usuarios", require("./routes/usuarios"));
+app.use("/api/auth", require("./routes/auth"));
 
 //definir la pagina principal
 app.get("/", (req, res) => {
