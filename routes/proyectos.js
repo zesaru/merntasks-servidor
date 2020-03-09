661f1f5b574df4c069fc78df54ccc0d1b71bf9e6
Cesar Murillo <namiayumis@gmail.com>
2020-03-09 15:52:18 +0900

    Obtener Proyectos en JSONç

--- routes/proyectos.js
+++ routes/proyectos.js
@@ -17,6 +17,6 @@ router.post(
   proyectoController.crearProyecto
 );
 
-router.get("/", auth, proyectoController.crearProyecto);
+router.get("/", auth, proyectoController.obtenerProyectos);
 
 module.exports = router;
