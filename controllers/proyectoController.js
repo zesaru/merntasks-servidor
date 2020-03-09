cbc3c631bbc11ab9b1ae46aaa6e573c85fd0bd2d
Cesar Murillo <namiayumis@gmail.com>
2020-03-09 16:16:12 +0900

    actualizar proyectos

--- controllers/proyectoController.js
+++ controllers/proyectoController.js
@@ -58,13 +58,22 @@ exports.actualizarProyecto = async(req, res) => {
 
     try {
 
-        // revisar el ID 
+        let proyecto = await Proyecto.findById(req.params.id);
 
         // si el proyecto existe o no
+        if(!proyecto) {
+            return res.status(404).json({msg: 'Proyecto no encontrado'})
+        }
 
         // verificar el creador del proyecto
+        if(proyecto.creador.toString() !== req.usuario.id ) {
+            return res.status(401).json({msg: 'No Autorizado'});
+        }
 
         // actualizar
+        proyecto = await Proyecto.findByIdAndUpdate({ _id: req.params.id }, { $set : nuevoProyecto}, { new: true });
+
+        res.json({proyecto});
 
     } catch (error) {
         console.log(error);
